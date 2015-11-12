1f621b049672067648548269b6eb216b6859df50
John Cole <john@jhcole.com>
2015-11-11 20:20:51 -0500

    Add documentation, bump version

--- lib/hola_jhcole.rb
+++ lib/hola_jhcole.rb
@@ -2,6 +2,15 @@ require "hola_jhcole/version"
 
 module HolaJhcole
   class Hola
+    # Say hi to the world!
+    #
+    # Example:
+    #    >> HolaJhcole::Hola.hi("spanish")
+    #    => hola mundo
+    #
+    # Arguments:
+    #   language: (String)
+
     def self.hi(language = "english")
       translator = Translator.new(language)
       translator.hi
--- lib/hola_jhcole/version.rb
+++ lib/hola_jhcole/version.rb
@@ -1,3 +1,3 @@
 module HolaJhcole
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
